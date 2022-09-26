dd5f44a754e19985d44a9a0fd4c8ff61750efdff
Guillermo <claseswilly@gmail.com>
2022-09-26 12:29:16 +0200

    initial commit

---
+++ src/main/java/com/everis/mvngff/App.java
@@ -0,0 +1,21 @@
+package com.everis.mvngff;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String str = "Hola Mundo de Nuevo";
+        System.out.println( StringUtils.lowerCase(str) );
+    }
+
+	
+    
+    
+}
